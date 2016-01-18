a2f88ecc392d2f59ea71c4829cb226f7fbff7b7e
BigFoot1989 <sven.allers@gmx.de>
2016-01-18 15:11:52 +0100

    Bugfix: Logger was not retrieved from Logger class

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -28,6 +28,12 @@ public class Main {
 	public static int NR_BITS_ID = 160;
 	public static BigInteger MAX_ID = BigInteger.valueOf(2).pow(NR_BITS_ID).subtract(BigInteger.ONE);
 	
+	/**
+	 * Gets the current chord instance in use. Creates a new one if none is
+	 * in use currently
+	 * @return
+	 * @throws ServiceException
+	 */
 	public static ChordImpl getChordInstance() throws ServiceException{
 		if (chord == null){
 			PropertiesLoader.loadPropertyFile();
@@ -36,6 +42,10 @@ public class Main {
 		return chord;
 	}
 	
+	/**
+	 * Loads the property file and puts its values into the property map
+	 * @throws IOException
+	 */
 	private static void loadPropertiesInstance() throws IOException{
 		if(propertyMap == null){
 			propertyMap = new HashMap<String,String>();
@@ -49,12 +59,18 @@ public class Main {
 		}
 	}
 	
+	/**
+	 * Returns the logger with the given name and adds a file handler if
+	 * an output file is set at the properties
+	 * @param name
+	 * @return
+	 */
 	public static Logger getLogger(String name){
-		Logger logger = loggerMap.get(name);
+		Logger logger = Logger.getLogger(name);
 		if(logger == null){
 			logger = Logger.getLogger(name);
 			if(logFile != null && logFile.length() > 0){
-				try {
+				try {//Add FileHandler if outputfile is set
 					logger.addHandler(new FileHandler(logFile));
 				} catch (SecurityException e) {
 					e.printStackTrace();
@@ -67,6 +83,12 @@ public class Main {
 		return logger;
 	}
 	
+	/**
+	 * Retrieves the property with the given name
+	 * @param propertyName
+	 * @return
+	 * @throws IOException
+	 */
 	public static String getProperty(String propertyName) throws IOException{
 		loadPropertiesInstance();
 		return propertyMap.get(propertyName);	
@@ -74,18 +96,19 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, MalformedURLException, ServiceException {
 		try{
-			if(args.length > 0){
+			if(args.length > 0){//Overwrite default property file if another one is given by the player
 				propertyFile = args[0];
 			}
 			logFile = getProperty("logFile");
-			logger = getLogger(ChordImpl.class.getName());
+			logger = getLogger(Main.class.getName());
 			logger.info("Battleship is starting");
 			int shipsPerPlayer = Integer.parseInt(getProperty("shipsPerPlayer"));
 			logger.info("Ships per player: "+shipsPerPlayer);
 			int fieldsPerPlayer = Integer.parseInt(getProperty("fieldsPerPlayer"));
 			logger.info("Fields per player: "+fieldsPerPlayer);
 			String localUrlStr = getProperty("localURL");
-			URL localURL = new URL("ocsocket://"+localUrlStr+"/");
+			String protocol = URL.KNOWN_PROTOCOLS.get(URL.SOCKET_PROTOCOL);
+			URL localURL = new URL(protocol+"://"+localUrlStr+"/");
 			logger.info("Local URL: "+localUrlStr);
 
 			ChordImpl chord = getChordInstance();
@@ -93,7 +116,7 @@ public class Main {
 			chord.setCallback(sm);
 			String bootstrapUrlStr = getProperty("bootstrapURL");
 			if(bootstrapUrlStr != null && bootstrapUrlStr.length() > 0){
-				URL bootstrapUrl = new URL("ocsocket://"+bootstrapUrlStr);
+				URL bootstrapUrl = new URL(protocol+"://"+bootstrapUrlStr+"/");
 				logger.info("Connect to bootstrap node "+bootstrapUrl);
 				chord.join(localURL, bootstrapUrl);
 			}else{
