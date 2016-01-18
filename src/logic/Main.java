e8096273c9d604bee5726cd6454a306d48fe30b6
BigFoot1989 <sven.allers@gmx.de>
2016-01-18 13:49:58 +0100

    Logging

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -6,6 +6,9 @@ import java.net.MalformedURLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import de.uniba.wiai.lspi.chord.data.ID;
 import de.uniba.wiai.lspi.chord.data.URL;
@@ -17,8 +20,11 @@ public class Main {
 	
 
 	private static Map<String,String> propertyMap = null;
+	private static Map<String,Logger> loggerMap = null;
+	private static String logFile = null;
 	private static ChordImpl chord = null;
 	private static String propertyFile = "battleship.properties";
+	private static Logger logger = null;
 	public static int NR_BITS_ID = 160;
 	public static BigInteger MAX_ID = BigInteger.valueOf(2).pow(NR_BITS_ID).subtract(BigInteger.ONE);
 	
@@ -43,33 +49,62 @@ public class Main {
 		}
 	}
 	
+	public static Logger getLogger(String name){
+		Logger logger = loggerMap.get(name);
+		if(logger == null){
+			logger = Logger.getLogger(name);
+			if(logFile != null && logFile.length() > 0){
+				try {
+					logger.addHandler(new FileHandler(logFile));
+				} catch (SecurityException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			loggerMap.put(name, logger);
+		}
+		return logger;
+	}
+	
 	public static String getProperty(String propertyName) throws IOException{
 		loadPropertiesInstance();
 		return propertyMap.get(propertyName);	
 	}
 
 	public static void main(String[] args) throws IOException, MalformedURLException, ServiceException {
-		if(args.length > 0){
-			propertyFile = args[0];
-		}
-		ChordImpl chord = getChordInstance();
-		int shipsPerPlayer = Integer.parseInt(getProperty("shipsPerPlayer"));
-		int fieldsPerPlayer = Integer.parseInt(getProperty("fieldsPerPlayer"));
-		String localUrlStr = getProperty("localURL");
-		StatisticsManager sm = new StatisticsManager(chord, shipsPerPlayer, fieldsPerPlayer);
-		chord.setCallback(sm);
-		URL localURL = new URL("ocsocket://"+localUrlStr+"/");
-		String bootstrapUrlStr = getProperty("bootstrapURL");
-		if(bootstrapUrlStr != null && bootstrapUrlStr.length() > 0){
-			URL bootstrapUrl = new URL("ocsocket://"+bootstrapUrlStr);
-			chord.join(localURL, bootstrapUrl);
-		}else{
-			chord.create(localURL, ID.valueOf(MAX_ID));
-		}
-		if(chord.getID().toBigInteger().equals(MAX_ID)){
-			System.out.println("Press any key to start");
-			System.console().readLine();
-			sm.firstShoot();
-		}
+		try{
+			if(args.length > 0){
+				propertyFile = args[0];
+			}
+			logFile = getProperty("logFile");
+			logger = getLogger(ChordImpl.class.getName());
+			logger.info("Battleship is starting");
+			ChordImpl chord = getChordInstance();
+			int shipsPerPlayer = Integer.parseInt(getProperty("shipsPerPlayer"));
+			int fieldsPerPlayer = Integer.parseInt(getProperty("fieldsPerPlayer"));
+			String localUrlStr = getProperty("localURL");
+			StatisticsManager sm = new StatisticsManager(chord, shipsPerPlayer, fieldsPerPlayer);
+			chord.setCallback(sm);
+			URL localURL = new URL("ocsocket://"+localUrlStr+"/");
+			String bootstrapUrlStr = getProperty("bootstrapURL");
+			if(bootstrapUrlStr != null && bootstrapUrlStr.length() > 0){
+				URL bootstrapUrl = new URL("ocsocket://"+bootstrapUrlStr);
+				logger.info("Connect to bootstrap node "+bootstrapUrl);
+				chord.join(localURL, bootstrapUrl);
+			}else{
+				logger.info("Create bootstrap node on "+localURL);
+				chord.create(localURL, ID.valueOf(MAX_ID));
+			}
+			logger.info("Battleship started: Your ID is "+chord.getID());
+			if(chord.getID().toBigInteger().equals(MAX_ID)){
+				System.out.println("Press any key to start");
+				System.console().readLine();
+				sm.firstShoot();
+			}
+		} catch(Exception e){
+			logger.log(Level.SEVERE, "An exception occured. The apllication shuts down.", e);
+			throw(e);
+		}		
 	}
 }
