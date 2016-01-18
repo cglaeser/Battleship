9a3de62fbfaaf090d0f194eb92937aac3efd32ae
Christian Gläser <glaeser@7says.de>
2016-01-18 12:33:47 +0100

    Merge FillList

--- src/tests/FillList.java
+++ src/tests/FillList.java
@@ -14,10 +14,15 @@ public class FillList {
 	public void checkListSizeTest() throws ServiceException{
 		StatisticsManager sm = new StatisticsManager();
 		System.out.println("FieldWithShips: " + sm.getFieldsWithShips());
-		assertTrue(sm.getFieldsWithShips().size()==10);
-		
+		assertTrue(sm.getFieldsWithShips().size()==10);	
 	}
 	
+	@Test
+	public void checkWrongIndexDeltionTest() throws ServiceException{;
+		StatisticsManager sm = new StatisticsManager(Main.getChordInstance(), 25, 25);
+		System.out.println("FieldWithShips: " + sm.getFieldsWithShips());
+		assertTrue(sm.getFieldsWithShips().size()==25);		
+	}
 	//Sind Spieler Felder zugeordnet?
 	
 	public void checkSelfTest() throws ServiceException{
